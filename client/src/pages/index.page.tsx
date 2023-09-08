783eebe59fe2873da2de5a3c79ef88994698e89d
s1f102100793 <s1f102100793@iniad.org>
2023-09-08 20:24:34 +0900

    なんとなくcss

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -1,12 +1,21 @@
+import { Helmet } from 'react-helmet';
 import styles from './index.module.css';
 
 const Home = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>感情カラーパレット</h1>
-      <p className={styles.description}>あなたの感情を色で表現しましょう。</p>
-      {/* ここに他のコンポーネントや要素を追加可能 */}
-    </div>
+    <>
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <div className={styles.container}>
+        <h1 className={styles.title}>感情カラーパレット</h1>
+        <p className={styles.description}>あなたの感情を色で表現しましょう。</p>
+        {/* ここに他のコンポーネントや要素を追加可能 */}
+      </div>
+    </>
   );
 };
 
