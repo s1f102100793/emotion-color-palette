d2b3a5be55e37b833f005edb966880df7b547cc2
s1f102100793 <s1f102100793@iniad.org>
2023-09-09 01:54:30 +0900

    数字も選べるようになった

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -7,15 +7,22 @@ import styles from './index.module.css';
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
   const [colors, setColors] = useState<string[]>([]);
+  const [selectedValue, setSelectedValue] = useState<number>(4);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const valueAsNumber = Number(event.target.value);
+    setSelectedValue(valueAsNumber);
+    console.log('選択された値:', event.target.value);
+  };
+
   const handleSubmit = async () => {
     setColors([]);
     console.log(inputValue);
-    const res = await apiClient.color.$post({ body: { text: inputValue, number: 5 } });
+    const res = await apiClient.color.$post({ body: { text: inputValue, number: selectedValue } });
     console.log(res);
     if (res !== null && res !== undefined) {
       const colorsArray = Object.values(res);
@@ -42,6 +49,12 @@ const Home = () => {
           onChange={handleInputChange}
           placeholder="感情を表現する文を入力..."
         />
+        <label htmlFor="colorCount">色の数を選択:</label>
+        <select id="colorCount" value={selectedValue} onChange={handleChange}>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+        </select>
         <button onClick={handleSubmit}>送信</button>
         <div className={styles.colors}>
           {' '}
