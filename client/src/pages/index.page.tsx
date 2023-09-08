dc7b6fe51c29db8f6d24b748ae4b2bf850fe864b
s1f102100793 <s1f102100793@iniad.org>
2023-09-08 18:10:55 +0900

    なんとなく作成

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -1,57 +1,12 @@
-import type { TaskModel } from 'commonTypesWithClient/models';
-import type { ChangeEvent, FormEvent } from 'react';
-import { useEffect, useState } from 'react';
-import { apiClient } from 'src/utils/apiClient';
-import { returnNull } from 'src/utils/returnNull';
 import styles from './index.module.css';
 
 const Home = () => {
-  // const [user] = useAtom(userAtom);
-  const [tasks, setTasks] = useState<TaskModel[]>();
-  const [label, setLabel] = useState('');
-  const inputLabel = (e: ChangeEvent<HTMLInputElement>) => {
-    setLabel(e.target.value);
-  };
-  const fetchTasks = async () => {
-    const tasks = await apiClient.tasks.$get().catch(returnNull);
-
-    if (tasks !== null) setTasks(tasks);
-  };
-  const createTask = async (e: FormEvent) => {
-    e.preventDefault();
-    if (!label) return;
-
-    await apiClient.tasks.post({ body: { label } });
-    setLabel('');
-    await fetchTasks();
-  };
-  const toggleDone = async (task: TaskModel) => {
-    await apiClient.tasks._taskId(task.id).patch({ body: { done: !task.done } });
-    await fetchTasks();
-  };
-  const deleteTask = async (task: TaskModel) => {
-    await apiClient.tasks._taskId(task.id).delete();
-    await fetchTasks();
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
-
-  // if (!tasks || !user) return <Loading visible />;
-
   return (
-    <>
-      {/* <BasicHeader user={user} /> */}
-      <div className={styles.title} style={{ marginTop: '160px' }}>
-        Welcome to frourio!
-      </div>
-
-      <form style={{ textAlign: 'center', marginTop: '80px' }} onSubmit={createTask}>
-        <input value={label} type="text" onChange={inputLabel} />
-        <input type="submit" value="ADD" />
-      </form>
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.title}>感情カラーパレット</h1>
+      <p className={styles.description}>あなたの感情を色で表現しましょう。</p>
+      {/* ここに他のコンポーネントや要素を追加可能 */}
+    </div>
   );
 };
 
