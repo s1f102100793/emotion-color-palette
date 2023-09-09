f638b2e6d0fcb563d64c1083fcaed57562fb93c3
s1f102100793 <s1f102100793@iniad.org>
2023-09-09 19:29:22 +0900

    バックエンドの関数はなんとか繋がった

--- server/repository/colorRepository.ts
+++ server/repository/colorRepository.ts
@@ -1,20 +1,37 @@
 import { prismaClient } from '$/service/prismaClient';
+import { toColorModel } from '$/useCase/colorUseCase';
 
-export const getItems = async (type: string) => {
+export const getItems = async (type: string, list: string[] | number) => {
   switch (type) {
     case 'number':
-      await getItemsFromNumber();
+      if (typeof list === 'number') {
+        await getItemsFromNumber(list);
+      }
       break;
     case 'color':
-      await getItemsFromColor();
+      if (Array.isArray(list)) {
+        await getItemsFromColor(list);
+      }
       break;
   }
 };
 
-export const getItemsFromNumber = async () => {
-  const prismaColor = await prismaClient.color.findMany({});
+export const getItemsFromNumber = async (paletteSize: number) => {
+  const prismaColor = await prismaClient.color.findMany({
+    where: { paletteSize },
+    select: { id: true, createdAt: true, txet: true, paletteSize: true, color: true, like: true },
+  });
+  return prismaColor.map(toColorModel);
 };
 
-export const getItemsFromColor = async () => {
-  const prismaColor = await prismaClient.color.findMany({});
+export const getItemsFromColor = async (color: string[]) => {
+  const prismaColor = await prismaClient.color.findMany({
+    where: {
+      color: {
+        hasSome: color,
+      },
+    },
+    select: { id: true, createdAt: true, txet: true, paletteSize: true, color: true, like: true },
+  });
+  return prismaColor.map(toColorModel);
 };
