11f0357cc268c6ce2437c24b866426b1db9ec30e
s1f102100793 <s1f102100793@iniad.org>
2023-09-09 12:35:48 +0900

    文字フォント変更

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -35,11 +35,14 @@ const Home = () => {
   return (
     <>
       <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@100&display=swap"
           rel="stylesheet"
         />
       </Helmet>
+
       <div className={styles.container}>
         <h1 className={styles.title}>感情カラーパレット</h1>
         <p className={styles.description}>あなたの感情を色で表現しよう。</p>
