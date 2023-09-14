b77ba74013eca44fcec3af20f35848a5bcc13e0b
s1f102100793 <s1f102100793@iniad.org>
2023-09-14 19:47:09 +0900

    Header作成

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { pagesPath } from 'src/utils/$path';
+import styles from './Header.module.css';
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  return (
+    <header className={`${styles.header} ${isScrolled ? styles.scrolled : ''}`}>
+      <span>感情カラーパレット</span>
+      <Link href={pagesPath.$url()}>
+        <span>作成する</span>
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
